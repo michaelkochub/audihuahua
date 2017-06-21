338d9bc1ae6e937e9a1aaaf096bdf37d4011d3e3
Kochubeevsky, Michael <MKochubeevsky@cvent.com>
2017-06-20 22:26:04 -0400

    basic idea working

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from app import app
+from flask import render_template
+import pafy
 
-@app.route("/")
-def hello_world():
-  return 'Hello, World!'
+@app.route("/audio/<video_id>")
+def hello_world(video_id=None):
+  url = 'https://www.youtube.com/watch?v={video_id}'.format(video_id=video_id);
+  video = pafy.new(url);
+  stream_url = video.getbestaudio(preftype='m4a').url;
+  return render_template('audio_template.html', audio_stream=stream_url)
+
